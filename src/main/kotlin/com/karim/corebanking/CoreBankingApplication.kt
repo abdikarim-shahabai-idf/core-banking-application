e38474373396225e3096a8717adacc8eeb46dcb0
Abdikarim Shakhabay <abdikarim.shahabaj@solvo.kz>
2023-07-26 16:38:30 +0600

    feat(init): initial commit

---
+++ src/main/kotlin/com/karim/corebanking/CoreBankingApplication.kt
@@ -0,0 +1,11 @@
+package com.karim.corebanking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreBankingApplication
+
+fun main() {
+	runApplication<CoreBankingApplication>()
+}
---
+++ src/test/kotlin/com/karim/corebanking/CoreBankingApplicationTests.kt
@@ -0,0 +1,3 @@
+package com.karim.corebanking
+
+
